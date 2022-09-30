a33755c7be74461a402e085e71ac9324acdb3651
Saathveekan Satheeshkumar <saathveekan.satheeshkumar@kcl.ac.uk>
2022-09-30 20:44:34 +0100

    first attempt at exercise

--- median.py
+++ median.py
@@ -1,6 +1,8 @@
 """Median calculator."""
 """ENTER YOUR SOLUTION HERE!"""
 
+import math
+
 while True:
     try:
         print("Enter a list of numbers separated by commas: ")
@@ -9,4 +11,15 @@ while True:
         print("Some input could not be converted to a number!")
     else:
         break
+
+
+numbers.sort()
 print(numbers)
+l = len(numbers)
+print(l)
+if not( (l % 2) == 0):
+   print(numbers[int(floor(l/2))])
+   print(l)
+else:
+    print(l)
+    print( (numbers[int(l/2) ] + numbers[int( (l/2)-1 ) ] )/2  )  
